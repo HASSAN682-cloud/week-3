e7abab5e32fa2a54247f152e83654c77d8dfb4a5
HASSAN M SAID <h.said@mombasawater.co.ke>
2025-08-16 00:20:49 +0300

    Add files via upload

---
+++ nsert at least 3 records into the student table. Each record should have a unique name, age, and gender..sql
@@ -0,0 +1,3 @@
+INSERT INTO student (name, age, gender) VALUES ('Alice', 20, 'Female');
+INSERT INTO student (name, age, gender) VALUES ('Bob', 22, 'Male');
+INSERT INTO student (name, age, gender) VALUES ('Charlie', 21, 'Male');
