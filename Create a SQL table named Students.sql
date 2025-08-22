0b986136543a82a949aa7091bbac6f3180c0daf9
HASSAN M SAID <h.said@mombasawater.co.ke>
2025-08-22 15:45:59 +0300

    Update Create a SQL table named Students.sql

--- Create a SQL table named Students.sql
+++ Create a SQL table named Students.sql
@@ -1,7 +1,25 @@
-CREATE DATABASE student;
-CREATE TABLE Students (
-  StudentID INT PRIMARY KEY,
-  Name VARCHAR(100),
-  Age INT,
-  Course VARCHAR(50)
-);
+-- Question 1
+
+CREATE INDEX IdxTotalAmount ON bills(TotalAmount);
+
+-- Question 2
+
+DROP INDEX IdxEmail ON customer;
+
+ 
+
+-- Question 3
+
+CREATE USER 'bob'@'localhost' IDENTIFIED BY 'S$cu3r3!';
+
+ 
+
+-- Question 4
+
+GRANT INSERT ON bills.* TO 'bob'@'localhost';
+
+ 
+
+-- Question 5
+
+ALTER USER 'bob'@'localhost' IDENTIFIED BY 'P$55!23';
